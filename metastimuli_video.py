0e4f4cde3621db41298c349d976b081e514627dc
Rico Picone <ricopicone@gmail.com>
2021-09-08 03:10:25 -0700

    basic transitions working

--- metastimuli_video.py
+++ metastimuli_video.py
@@ -4,11 +4,23 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import platform
+import re
 
 class metavid:
 	def __init__(self, filename):
 		self.filename = pl.Path(filename)
 		self.stream = ffmpeg.input(os.fspath(self.filename))
+		self.timestamps = [] # populate with get_scene_transitions
+
+	def run(self, stream, filename='_dummy.mp4'):
+		system = platform.system()
+		if system == 'Darwin':
+			print('using macOS hardware acceleration')
+			stream = ffmpeg.output(stream,filename=filename,vcodec='h264_videotoolbox')
+		else:
+			stream = ffmpeg.output(stream,filename=filename)
+		ffmpeg.run(stream)
+		return self
 
 	def export(self, filename=None):
 		if not filename:
@@ -17,29 +29,61 @@ class metavid:
 		else:
 			filename = pl.Path(filename)
 		filename = os.fspath(filename)
-		system = platform.system()
-		if system == 'Darwin':
-			print('using macOS hardware acceleration')
-			self.stream = ffmpeg.output(self.stream,filename,vcodec='h264_videotoolbox')
-		else:
-			self.stream = ffmpeg.output(self.stream, filename)
-		ffmpeg.run(self.stream)
+		self.run(self.stream, filename)
 		return self
 
 	def hflip(self):
 		self.stream = ffmpeg.hflip(self.stream)
 		return self
 
-	def get_scene_transitions(self):
-		self.stream = ffmpeg.filter(self.stream,'select','gt(scene,0.05)')
-		self.stream = ffmpeg.filter(self.stream,'metadata','print',file=self.filename.stem+'_transitions.txt')
+	def get_scene_transitions(self,force=False):
+		# saves metadata timestamps of transitions
+		transitions_file = self.filename.stem+'_transitions.txt'
+		if not pl.Path(transitions_file).is_file() or force:
+			stream = ffmpeg.filter(self.stream,'select','gt(scene,0.04)')
+			stream = ffmpeg.filter(stream,'metadata','print',file=transitions_file)
+			self.run(stream)
+		self.timestamps_extract(transitions_file)
 		return self
 
 	def overlay_plot(self,image,time_range=[0,10]):
+		# overlays a single image on self.stream over time_range
 		image_stream = ffmpeg.input(os.fspath(image))
 		self.stream = ffmpeg.overlay(
 			self.stream,
 			image_stream,
 			enable=f'between(t,{time_range[0]},{time_range[1]})'
 		)
+		return self
+
+	def overlay_plot_i(self,image,atom_index):
+		# overlays a single image on self.stream over time range corresponding to atom_index
+		time_range = self.time_range_i(atom_index)
+		self.overlay_plot(image,time_range)
+
+	def time_range_i(self,atom_index):
+		if not self.timestamps:
+			raise Exception('no timestamps yet: run get_scene_transitions first')
+		else:
+			if atom_index == 0:
+				low = 0.0
+			else:
+				low = self.timestamps[atom_index]
+			if atom_index+1 < len(self.timestamps):
+				high = self.timestamps[atom_index+1]
+			else:
+				high = self.timestamps[-1]
+			return [low,high]
+
+	def overlay_all_plots(self,image_dict):
+		return self
+
+	def timestamps_extract(self,tfile):
+		# extracts timestamps from tfile from metadata filter in get_scene_transitions method
+		self.timestamps = []
+		for line in open(tfile):
+			match = re.search('pts_time:([+-]?([0-9]*[.])?[0-9]+)', line)
+			if match:
+				print(match.group(1))
+				self.timestamps.append(match.group(1))
 		return self
--- variables.py
+++ variables.py
@@ -4,31 +4,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 from metastimuli_video import *
 
-# make a plot
-
-np.random.seed(19680801)
-x = np.arange(0.0, 50.0, 2.0)
-y = x ** 1.3 + np.random.rand(*x.shape) * 30.0
-s = np.random.rand(*x.shape) * 800 + 500
-
-plt.scatter(x, y, s, c="g", alpha=0.5, marker=r'$\clubsuit$',
-            label="Luck")
-plt.xlabel("Leprechauns")
-plt.ylabel("Gold")
-plt.legend(loc='upper left')
-plt.savefig("lucky.png", transparent=True)
-
-# overlay it on video
-
-mv = metavid('variables.mp4')
-# mv.overlay_plot(
-# 	"lucky.png", # image file
-# 	[0,22] # time range (seconds)
-# )
-# mv.export()
-
 # extract scene transitions
 
 mv = metavid('variables.mp4')
-mv.get_scene_transitions()
-mv.export('scene_transitioned.mp4')
+mv.get_scene_transitions() # this saves into mv.timestamps
+
+# import projected atom arrays
+
+# should import from Dane's work but generating for now
+np.random.seed(19680801)
+n_atoms = 9
+atoms = np.random.rand(2,n_atoms) * 10.0
+
+# make plots
+
+for i in range(0,n_atoms):
+	plt.scatter(atoms[0,:],atoms[1,:], c="g", marker=r'$\clubsuit$')
+	plt.scatter(atoms[0,i],atoms[1,i], c="r", marker=r'$\clubsuit$')
+	plt.savefig(f'lucky_{i}.png', transparent=True)
+
+# overlay on video
+
+# mv = metavid('variables.mp4')
+for i in range(0,n_atoms):
+	mv.overlay_plot_i(
+		f'lucky_{i}.png', # image file
+		i # atom index
+	)
+mv.export()
+
+# mv = metavid('variables.mp4')
+# mv.timestamps_extract('variables_transitions.txt')
