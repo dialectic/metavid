ed6f4b2ef523171e62d8a3db1a60a3a0fe8a5361
Rico Picone <ricopicone@gmail.com>
2021-09-08 14:07:57 -0700

    loading actual data atoms

--- variables.py
+++ variables.py
@@ -3,6 +3,7 @@ import pathlib as pl
 import matplotlib.pyplot as plt
 import numpy as np
 from metastimuli_video import *
+import pickle
 
 # extract scene transitions
 
@@ -11,16 +12,14 @@ mv.get_scene_transitions() # this saves into mv.timestamps
 
 # import projected atom arrays
 
-# should import from Dane's work but generating for now
-np.random.seed(19680801)
-n_atoms = 9
-atoms = np.random.rand(2,n_atoms) * 10.0
+atoms = pickle.load(open( "chi_pred.pkl", "rb" )).transpose()
+n_atoms = np.shape(atoms)[1]
 
 # make plots
 
 for i in range(0,n_atoms):
-	plt.scatter(atoms[0,:],atoms[1,:], c="g", marker=r'$\clubsuit$')
-	plt.scatter(atoms[0,i],atoms[1,i], c="r", marker=r'$\clubsuit$')
+	plt.scatter(atoms[0,:],atoms[1,:], c="g", marker='o')
+	plt.scatter(atoms[0,i],atoms[1,i], c="r", marker='o')
 	plt.savefig(f'lucky_{i}.png', transparent=True)
 
 # overlay on video
@@ -31,7 +30,7 @@ for i in range(0,n_atoms):
 		f'lucky_{i}.png', # image file
 		i # atom index
 	)
-mv.export()
+# mv.export()
 
 # mv = metavid('variables.mp4')
 # mv.timestamps_extract('variables_transitions.txt')
