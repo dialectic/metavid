a102e645f3464839c6eed5de85a13ea2b6acc82b
Alexander Benson <65480364+pluralize-abenson@users.noreply.github.com>
2021-09-09 23:44:42 -0700

    Update variables.py

--- example/variables.py
+++ example/variables.py
@@ -21,9 +21,6 @@ mv.load_atoms('chi_pred.pkl')
 mv.overlay_all_plots(
 	starting_scene_i=1, fig_type = 3, filename_base='testFigs/plot_', file_type = 'png'# so first (i=0) scene doesn't get a plot
 )
-
-mv.plot(filename_base='testFigs/singleOut/plot_ty_1',fig_type=1, file_type='pdf')
-mv.plot(filename_base='testFigs/singleOut/plot_ty_2',fig_type=2,file_type = 'pdf')
 # run video processing and export video
 
 # mv.export()
